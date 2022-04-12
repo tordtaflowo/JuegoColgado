b226ab2f2a718faad634346c49f9c406804b67ad
tordtaflowo <103199170+tordtaflowo@users.noreply.github.com>
2022-04-12 11:08:43 -0400

    Add files via upload

---
+++ el colgado2.py
@@ -0,0 +1,11 @@
+print("JUEGO DEL COLGADO")
+print("TIENE 6 VIDAS")
+print("(ingrese las letras en minuscula)")
+print("_ _ _ _ _ _ _ _ _ \n")
+## la palabra es programar
+
+letra1 = str(input("ingrese una letra: "))
+
+if letra1 == "p" :
+    print("\nLA LETRA ES CORRECTA")
+    print("P _ _ _ _ _ _ _ _ \n")
